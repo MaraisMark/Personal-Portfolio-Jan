3d9848f8d81359471364f396843cc82097cc3238
MaraisMark <76942024+MaraisMark@users.noreply.github.com>
2021-01-27 12:01:38 +0800

    Add files via upload
    This is my personal portfolio as of Jan

---
+++ Portfolio2/mark.js
@@ -0,0 +1,13 @@
+// $("#no-butt").click(function () {
+//   alert("I TOLD YOU NOTHING HAPPENS!!");
+// });
+
+$("#no-butt").click(function () {
+  $(".cover").toggleClass("covernew");
+  $("#firsth").toggleClass("firsth22");
+  $("#buddy").toggleClass("firsth23");
+});
+
+// $("#no-butt").click(function(){
+//   // Change src attribute of image
+//   $("#one").attr("src", "img/image2.jpg");
